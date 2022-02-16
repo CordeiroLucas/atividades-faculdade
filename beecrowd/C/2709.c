123762cfc1b64dba678f88a8d7c6ec26fd39bc1d
CordeiroLucas <lcp2@cesar.school>
2022-02-16 16:41:33 -0300

    new activities

--- beecrowd/C/2709.c
+++ beecrowd/C/2709.c
@@ -4,38 +4,41 @@ int main() {
     int nMoedas, i, salto, soma, isPrime;
     
     while (scanf(" %d", &nMoedas) != EOF) {
-            i = 0;
+        if (nMoedas >= 2 && nMoedas <= 20) {
+            i = 1;
     
             int valorMoedas[nMoedas];
             
-            while (i < nMoedas) {
+            while (i <= nMoedas) {
                 scanf(" %d", &valorMoedas[i]);
                 i++;
             };
             
             soma = 0;
-            i = 0;
+            i = nMoedas;
             
             scanf(" %d", &salto);
-            
-            do {
-                soma = soma + valorMoedas[i];
-                i = i + salto;
-            } while (i < nMoedas);
-            
-            isPrime = 0;
-            
-            for (i = 2; i < soma/2; i++) {
-                if (soma % i == 0) {
-                    isPrime++;
-                    break;
+            if (salto >= 1 && salto <= nMoedas) {
+                do {
+                    soma = soma + valorMoedas[i];
+                    i = i - salto;
+                } while (i >= 1);
+                
+                isPrime = 0;
+                
+                for (i = 2; i < soma/2; i++) {
+                    if (soma % i == 0) {
+                        isPrime++;
+                        break;
+                    };
                 };
-            };
-            
-            if (isPrime == 0)
-                printf("You’re a coastal aircraft, Robbie, a large silver aircraft.\n");
-            else 
-                printf("Bad boy! I’ll hit you.\n");
+                
+                if (isPrime == 0)
+                    return(printf("You’re a coastal aircraft, Robbie, a large silver aircraft.\n"));
+                else 
+                    printf("Bad boy! I’ll hit you.\n");
+            }
+        }
     }
  
     return 0;
