5e05a83bf434ec8862c501ee018ba8c434a91a96
Lucas Cordeiro Pacheco <pacheco.lucascordeiro@gmail.com>
2022-09-06 16:41:02 -0300

    Update server.py

--- aulas/icm/socket/server.py
+++ aulas/icm/socket/server.py
@@ -1,26 +1,28 @@
 import socket
+import threading
 
 serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serv.bind(("192.168.0.10", 8080))
 serv.listen((5))
 
-
-full_msg = ''
 while True:
         clientsocket, address = serv.accept()
         print(f"Connection from {address} has been established!")
 
         clientsocket.send(bytes("Welcome to the Server!", "utf-8"))
-        
+        i = 0
         while True:
                 msg = clientsocket.recv(1024)
                 full_msg = msg.decode("utf-8")
                 
-                if msg.decode("utf-8") == "quit":
+                if full_msg == "quit":
                         clientsocket.close()
                         print(f"Client {address} disconnected!\n")
                         break
-                else: print(full_msg)
+                else:
+                        print(f"Package {i}: {full_msg}")
+                        i+=1
+                
 
         
         
