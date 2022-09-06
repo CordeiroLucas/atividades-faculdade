dc0cf8629a0195c103dba6e4d2e9c7c6f14b5230
CordeiroLucas <lcp2@cesar.school>
2022-09-06 16:21:29 -0300

    Update server.py

--- aulas/icm/socket/server.py
+++ aulas/icm/socket/server.py
@@ -8,11 +8,6 @@ full_msg = ''
 while True:
         clientsocket, address = serv.accept()
         print(f"Connection from {address} has been established!")
-<<<<<<< HEAD
-        clientsocket.send(bytes("Welcome to the Server!\n", "utf-8"))
-
-        clientsocket.close()
-        print(f"Client {address} disconnected!\n")
 =======
         clientsocket.send(bytes("Welcome to the Server!", "utf-8"))
         
