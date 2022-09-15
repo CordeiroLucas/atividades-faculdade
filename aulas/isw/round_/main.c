42df5a36218ad46a45d0377622d26db65902d4f2
CordeiroLucas <lcp2@cesar.school>
2022-09-15 11:23:17 -0300

    update

--- aulas/isw/round_/main.c
+++ aulas/isw/round_/main.c
@@ -8,27 +8,37 @@ void main(){
 	int time = 0;
 	int i = 0;
 	int count = 3;
-	int NOP = count;
+	int NOP = count;//Number of Process
 	int q = 2;
 
-	printf("\nProcess No \t\t Burst \t\t Turn Around\n");
+	int last;
+
+	printf("\nProcess No \t\t Burst \t\t Turn Around");
 	while(count != 0)
 	{
-		if(burst_cp[i] > 0 && init[i] <= time){
+		if(burst_cp[i] > 0 && init[i] <= time)
+		{
 			if(burst_cp[i] <= q)
 			{
 				count--;
 				time += burst_cp[i];
 				burst_cp[i] = 0;
 
-				printf("Process[%d] \t\t %d \t\t %d", i, burst[i], time - init[i]);
+				printf("\nProcess[%d] \t\t %d \t\t %d", i, burst[i], time - init[i]);
 			}
 			else
 			{
 				burst_cp[i] -= q;
 				time += q;
 			}
+			last = i;
+		}
+		else
+		{
+			if(i == last)
+				time++;
 		}
 		i = (i+1)%NOP;
 	}
+	printf("\n");
 }
