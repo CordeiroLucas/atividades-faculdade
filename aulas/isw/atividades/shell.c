f9078fdc090058e33d00d04e78e9925dbd7666ea
Lucas Cordeiro Pacheco <pacheco.lucascordeiro@gmail.com>
2022-09-21 16:32:42 -0300

    a

--- aulas/isw/atividades/shell.c
+++ aulas/isw/atividades/shell.c
@@ -1,24 +1,44 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
 
 #define MAX_LINE 80 /* 80 chars per line, per command */
 
 int main(void)
 {
-    char *args[MAX_LINE/2 + 1];	/* command line has max of 40 arguments */
-    int should_run = 1;		/* flag to help exit program*/
-	
-    while (should_run) {
+	char* args[MAX_LINE/2 + 1];	/* command line has max of 40 arguments */
+	int should_run = 1;		/* flag to help exit program*/
+	pid_t pid;
+		
+	while (should_run) {
 
-	printf("est seq> ");
-        fflush(stdout);
+		printf("lcp2 seq> ");
+		fflush(stdout);
 
-	/*
-	 After read the input, the next steps are:
-	    - create n child process with fork() command
-	    - the child process will invoke execvp()
-	    - parent will invoke wait() unless command line include &	 
-	 */
-    }
-	return 0;
+		/*
+		After read the input, the next steps are:
+		- create n child process with fork() command
+		- the child process will invoke execvp()
+		- parent will invoke wait() unless command line include &	 
+		*/
+
+		fgets(args, MAX_LINE/2 + 1, stdin);
+
+		pid = fork();
+
+		if (pid < 0) {
+			fprintf(stderr, "Fork failed\n");
+			return 1;
+		} 
+		else if (pid == 0) {
+
+			
+		}
+
+		if (strcmp(args, "exit\n") == 0) exit(0);
+
+		printf("%s", args);
+	}
+		return 0;
 }
